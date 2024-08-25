d0b61cd4daa8b4166c7345f32453a38bb15adbf5
IesusEmanuel <52183235+IesusEmanuel@users.noreply.github.com>
2024-08-25 00:59:09 +0000

    Alterações

--- src/components/Texts/texts.jsx
+++ src/components/Texts/texts.jsx
@@ -3,10 +3,11 @@ import "/src/App.css";
 
 const Text = ({ text }) => {
   return (
-    <h1 className="margin poppins uppercase size-3 center">
+    <h1 style={{ fontFamily: 'Higher, sans-serif', letterSpacing: '3px' }}className="margin uppercase size-3 center">
       { text }
     </h1>
   )
 }
+
 
 export default Text;
